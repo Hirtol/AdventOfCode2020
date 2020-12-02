4e0ff660f742be6d1609c6246ef7f6d3f58168e6
Valentijn Hol <valentijnhol@hotmail.nl>
2020-12-02 17:30:36 +0100

    Move everything to a common trait

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,52 +1,56 @@
-use advent_of_code_2020::{read_puzzle_file, BasicOptions};
+use advent_of_code_2020::{read_puzzle_file, BasicOptions, AdventOfCode};
 use clap::Clap;
 
 use std::option::Option::Some;
 use std::path::PathBuf;
 
-fn main() -> anyhow::Result<()> {
-    let opts: BasicOptions = BasicOptions::parse();
-    let path = PathBuf::from(opts.path_to_puzzles).join("day_1.txt");
-    let puzzle = read_puzzle_file(path)?
-        .into_iter()
-        .flat_map(|s| s.parse())
-        .collect::<Vec<u64>>();
+struct Day1;
+impl AdventOfCode for Day1 {
+    type PuzzleData = Vec<u64>;
 
-    let first_result = find_two_numbers_for_2020(&puzzle, 2020).unwrap();
-    let second_result = find_three_numbers_for_2020(&puzzle).unwrap();
+    fn part_one(puzzle: &Self::PuzzleData) {
+        let first_result = find_two_numbers_for_2020(puzzle, 2020).unwrap();
+        println!(
+            "First Result for ints ({}, {}) is: {}",
+            first_result.0,
+            first_result.1,
+            first_result.0 * first_result.1
+        );
+    }
 
-    println!(
-        "First Result for ints ({}, {}) is: {}",
-        first_result.0,
-        first_result.1,
-        first_result.0 * first_result.1
-    );
-    println!(
-        "Second Result for ints ({}, {}, {}) is: {}",
-        second_result.0,
-        second_result.1,
-        second_result.2,
-        second_result.0 * second_result.1 * second_result.2
-    );
+    fn part_two(puzzle: &Self::PuzzleData) {
+        for first_int in puzzle {
+            if let Some((second, third)) = find_two_numbers_for_2020(puzzle, 2020 - first_int) {
+                println!(
+                    "Second Result for ints ({}, {}, {}) is: {}",
+                    first_int,
+                    second,
+                    third,
+                    first_int * second * third
+                );
+                break;
+            }
+        }
+    }
 
-    Ok(())
+    fn parse_file(puzzle: Vec<String>) -> Self::PuzzleData {
+        puzzle.into_iter()
+            .flat_map(|s| s.parse())
+            .collect::<Vec<u64>>()
+    }
 }
 
 fn find_two_numbers_for_2020(puzzle: &[u64], expected: u64) -> Option<(u64, u64)> {
     for first_int in puzzle {
-        let temp_result = puzzle.iter().cloned().find(|to_add| to_add + first_int == expected);
-        if let Some(second) = temp_result {
-            return Some((*first_int, second));
+        let expect = expected - first_int;
+        if puzzle.contains(&expect) {
+            return Some((*first_int, expect))
         }
     }
     None
 }
 
-fn find_three_numbers_for_2020(puzzle: &[u64]) -> Option<(u64, u64, u64)> {
-    for first_int in puzzle {
-        if let Some((second, third)) = find_two_numbers_for_2020(puzzle, 2020 - first_int) {
-            return Some((*first_int, second, third));
-        }
-    }
-    None
+fn main() -> anyhow::Result<()> {
+    let opts: BasicOptions = BasicOptions::parse();
+    Day1::start(PathBuf::from(opts.path_to_puzzles).join("day_1.txt"))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,22 @@ use std::io::{BufRead, BufReader};
 use std::path::Path;
 
 pub trait AdventOfCode {
-    fn solution();
+    type PuzzleData;
+
+    fn start(puzzle_path: impl AsRef<Path>) -> anyhow::Result<()> {
+        let parsed_data = Self::parse_file(read_puzzle_file(puzzle_path)?);
+
+        println!("Starting part one!");
+        Self::part_one(&parsed_data);
+        println!("Starting part two!");
+        Self::part_two(&parsed_data);
+        println!("Finished");
+        Ok(())
+    }
+
+    fn part_one(puzzle: &Self::PuzzleData);
+    fn part_two(puzzle: &Self::PuzzleData);
+    fn parse_file(puzzle: Vec<String>) -> Self::PuzzleData;
 }
 
 #[derive(Clone, Debug, Default, Clap)]
