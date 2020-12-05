7b7f87f4c1da359bc68bb8aaee50cd1dfe44a496
Valentijn Hol <valentijnhol@hotmail.nl>
2020-12-05 18:38:30 +0100

    Clean up day 5.

--- src/bin/day_5.rs
+++ src/bin/day_5.rs
@@ -1,23 +1,10 @@
+use advent_of_code_2020::{AdventOfCode, BasicOptions};
+use clap::Clap;
+use itertools::Itertools;
 use std::path::PathBuf;
 
-use clap::Clap;
-
-use advent_of_code_2020::{AdventOfCode, BasicOptions};
-use itertools::Itertools;
-
+type SeatId = usize;
 struct Day5;
-
-fn binary_space(chars: impl Iterator<Item=char>, min_max: (usize, usize)) -> usize {
-    chars.fold(min_max, |(lower_bound, upper_bound), c| {
-        // Ceiling divide.
-        let delta = ((upper_bound - lower_bound) + 1) / 2;
-        match c {
-            'F' | 'L' => (lower_bound, upper_bound - delta),
-            'B' | 'R' => (lower_bound + delta, upper_bound),
-            _ => unreachable!()
-        }
-    }).0
-}
 
 struct Seat {
     row: usize,
@@ -26,32 +13,40 @@ struct Seat {
 
 impl Seat {
     pub fn new(code: impl AsRef<str>) -> Self {
-        let row = binary_space(code.as_ref().chars().take(7), (0, 127));
-        let column = binary_space(code.as_ref().chars().skip(7).take(3), (0, 7));
         Seat {
-            row,
-            column
+            row: binary_space(code.as_ref().chars().take(7), (0, 127)),
+            column: binary_space(code.as_ref().chars().skip(7).take(3), (0, 7)),
         }
     }
 
-    pub fn seat_id(self) -> usize {
+    pub fn seat_id(self) -> SeatId {
         self.row * 8 + self.column
     }
 }
 
+fn binary_space(chars: impl Iterator<Item = char>, min_max: (usize, usize)) -> usize {
+    chars
+        .fold(min_max, |(lower_bound, upper_bound), c| {
+            // Ceiling divide.
+            let delta = ((upper_bound - lower_bound) + 1) / 2;
+            match c {
+                'F' | 'L' => (lower_bound, upper_bound - delta),
+                'B' | 'R' => (lower_bound + delta, upper_bound),
+                _ => unreachable!(),
+            }
+        })
+        .0
+}
+
 impl AdventOfCode for Day5 {
-    type PuzzleData = Vec<usize>;
+    type PuzzleData = Vec<SeatId>;
 
     fn part_one(puzzle: &Self::PuzzleData) {
-        let max_id = puzzle.iter().max().unwrap();
-        println!("Max id: {}", max_id)
+        println!("Max id: {}", puzzle.iter().max().unwrap())
     }
 
     fn part_two(puzzle: &Self::PuzzleData) {
-        let missing_seat = puzzle
-            .windows(2)
-            .find(|values| values[0] + 1 != values[1])
-            .unwrap();
+        let missing_seat = puzzle.windows(2).find(|values| values[0] + 1 != values[1]).unwrap();
         println!("Missing seat: {}", missing_seat[0] + 1)
     }
 
@@ -67,11 +62,9 @@ fn main() -> anyhow::Result<()> {
 
 #[cfg(test)]
 mod tests {
-    use crate::Seat;
-
     #[test]
     pub fn parse_test() {
-        let seat = Seat::new("FBFBBFFRLR");
+        let seat = super::Seat::new("FBFBBFFRLR");
         assert_eq!(seat.row, 44);
         assert_eq!(seat.column, 5);
     }
